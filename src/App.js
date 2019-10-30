192170f513030912251bef862f982dfd9377c099
Jess Dam <Jess Dam>
2019-10-30 22:03:40 +0000

    added new component files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>Simon Says</h1>
+    <Instructions/>
+    <Game/>
+    <StartGame/>
+    </>
+  );
+}
+
+export default App;
