d5f6700c58d8fcc10692fc74229b640b708fbe6d
Jess Dam <Jess Dam>
2019-11-08 15:20:04 +0000

    created menu with routing

---
+++ src/components/HighScore.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HighScore(){
+    return(
+        <h2>Highscores</h2>
+    )
+}
+
+export default HighScore
---
+++ src/components/Menu.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Instructions from './Instructions'
+
+
+function Menu(){
+    return(
+        <>
+            <h1>Simon Says</h1>
+            <br></br>
+            <Instructions/>
+            <br></br>
+        </>
+
+    )
+}
+
+export default Menu
